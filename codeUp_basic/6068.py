71b5b69e54b2c17d89ca10934b153503bd1a6931
sera6 <sera6208@naver.com>
2022-02-05 11:41:34 +0900

    코드업 파이썬 기초(6066~6070)

---
+++ codeUp_basic/6068.py
@@ -0,0 +1,22 @@
+a = input()
+a = int(a)
+
+if a>=90 :
+    print("A")
+elif a>=70 :
+    print("B")
+elif a>=40 :
+    print("C")
+else :
+    print("D")
+
+# if a>=90 :
+#     print("A")
+# else :
+#     if a>=70 :
+#         print("B")
+#     else :
+#         if a>=40 :
+#             print("C")
+#         else :
+#             print("D")
